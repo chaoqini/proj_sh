27716dc255e357dae2e2aaca0409e79e0375f842
qc <chaoqini@qq.com>
2022-02-27 18:12:40 +0800

    20220227

---
+++ gitupdate.sh
@@ -0,0 +1,8 @@
+cd ~/.config
+git status
+git add "~/.config/*"
+#git commit -m '$message'
+#git commit -m $1
+git commit -a -m $1
+git push
+
---
+++ test.sh
@@ -0,0 +1,9 @@
+builtin cd $1
+git status
+echo 11
+git add "$1/."
+echo $1
+echo $@
+echo 22
+git commit -a -m $2
+echo 33
