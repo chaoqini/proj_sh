cd8bf54df8cbb38b6e950771c77479c6728a9068
qc <chaoqini@qq.com>
2022-02-27 20:17:02 +0800

    date

--- gitupdate.sh
+++ gitupdate.sh
@@ -2,10 +2,7 @@
 ##git remote add origin git@github.com:chaoqini/proj_sh.git
 ##git branch -M main
 ##git push --set-upstream origin main
-#date=`date +%Y%m%d`
 date=$(date +%Y%m%d)
-#echo $date
-#exit 
 message=${1:-date}
 dir=${2:-.}
 date=`date +%Y%m%d`
