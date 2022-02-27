e13caef70db7d0415947fb59334a35fe3bb24e64
qc <chaoqini@qq.com>
2022-02-27 18:52:32 +0800

    20220227g

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 ##git remote add origin git@github.com:chaoqini/proj_sh.git
 ##git branch -M main
 ##git push --set-upstream origin main
-builtin cd "$!"
-git add "$1/."
-git commit -a -m $2
+##builtin cd "$1"
+##git add "$1/."
+git add .
+git commit -a -m $1
 git push
