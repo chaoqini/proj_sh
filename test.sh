c27708b68d04a70f598123f3693caea6c8878aec
qc <chaoqini@qq.com>
2022-02-27 18:26:40 +0800

    20220227b

--- test.sh
+++ test.sh
@@ -1,14 +1,16 @@
-echo 00
-builtin cd $1
-git status
-echo $1
-echo 11
+##git remote add origin git@github.com:chaoqini/proj_sh.git
+##git push --set-upstream origin main
+#echo 00
+#builtin cd $1
+#git status
+#echo $1
+#echo 11
 git add "$1/."
-echo "$1/."
-echo 22
+#echo "$1/."
+#echo 22
 git commit -a -m $2
-echo $2
-echo 33
+#echo $2
+#echo 33
 git push
-echo 44
-echo $@
+#echo 44
+#echo $@
