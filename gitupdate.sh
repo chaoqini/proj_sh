d2fd762012f9d7994680a8c9c71a115822acd6ba
qc <chaoqini@qq.com>
2022-02-27 19:04:21 +0800

    sh_20220227

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,8 +1,9 @@
 ##git remote add origin git@github.com:chaoqini/proj_sh.git
 ##git branch -M main
 ##git push --set-upstream origin main
-builtin cd $1
-git add "$1/."
+dir=${2-.}
+builtin cd $dir
+git add "$dir/."
 #git add .
 git commit -a -m $1
 git push
