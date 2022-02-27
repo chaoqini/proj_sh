5b17b96989c1e029bfc7d1441c86dd6fcf3bd040
qc <chaoqini@qq.com>
2022-02-27 19:53:40 +0800

    null

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,9 +1,15 @@
+#!/bin/bash
 ##git remote add origin git@github.com:chaoqini/proj_sh.git
 ##git branch -M main
 ##git push --set-upstream origin main
-dir=${2-.}
+#date=`date +%Y%m%d`
+date=$(date +%Y%m%d)
+#echo $date
+#exit 
+message=${1:-date}
+dir=${2:-.}
+date=`date +%Y%m%d`
 builtin cd $dir
 git add "$dir/."
-#git add .
-git commit -a -m $1
+git commit -a -m $message
 git push
